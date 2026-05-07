48b1dee2fc558e44f1938f939376ddd70392c1f5
Bhaskar8130 <95397859+Bhaskar8130@users.noreply.github.com>
2026-05-07 17:27:58 +0530

    v1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Welcome to React 🚀</h1>
+
+        <p>
+          This is my first React application.
+        </p>
+
+        <button>Explore</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
